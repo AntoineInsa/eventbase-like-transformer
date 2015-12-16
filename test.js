16193235711720ac980cb793b86cf4bd5764e5b5
Antoine Censi <antoine.censi@gmail.com>
2015-12-16 12:24:29 -0800

    Add test cases

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var likes = require('./eventbase-likes-transformer.js');
 
+// Simulate AWS lambda context
 var context = {
     succeed: function (result) {
         console.log('------------');
@@ -14,14 +15,101 @@ var context = {
     }
 };
 
-// Simulated S3 bucket event
-var event = {
-  'api_host': 'localhost.attendease.com',
+// Simulate dates
+formatDate = function(date) {
+  date = date || new Date()
+  return date.toISOString().replace(/\.\d{3}/,'')
+}
+
+currentDate = function(offset) {
+  offset = offset || 0
+  now = new Date()
+  return formatDate(new Date(now.getTime() + offset*60000))
+}
+
+var offset = 0
+
+// Simulated S3 bucket events
+var eventTemplate = {
+  'api_host': 'attendease.com',
   'event_id': 'eventbase',
-  'attendee_token': 'this-is-a-secret-token',
-  //'version': '2014-01-01T15:22:55Z',
+  'token': 'this-is-a-secret-attendee-token',
   'schedule_list': []
 };
+
+// Advanced events
+var template = {
+  'api_host': 'attendease.com',
+  'event_id': 'eventbase',
+  'token': 'this-is-a-secret-attendee-token',
+  'object1': 'object1_id',
+  'object2': 'object2_id',
+  'version': currentDate(-1.1)
+}
+
+var events = {
+  'firstSync' : {
+    'api_host': template.api_host,
+    'event_id': template.event_id,
+    'token': template.token,
+    'schedule_list': []
+  },
+  'secondSync' : {
+    'api_host': template.api_host,
+    'event_id': template.event_id,
+    'token': template.token,
+    'version': currentDate(-1),
+    'schedule_list': []
+  },
+  'likeOneUnlikeTwo' : {
+    'api_host': template.api_host,
+    'event_id': template.event_id,
+    'token': template.token,
+    'version': template.version,
+    'schedule_list': [
+      // like one
+      {'object_id': template.object1,
+      'timestamp': currentDate(0),
+      'value': 1},
+      // unlike two
+      {'object_id': template.object2,
+      'timestamp': currentDate(0),
+      'value': 0}]
+  },
+  'unlikeOneLikeTwo' : {
+    'api_host': template.api_host,
+    'event_id': template.event_id,
+    'token': template.token,
+    'version': template.version,
+    'schedule_list': [
+      // unlike one
+      {'object_id': template.object1,
+      'timestamp': currentDate(0),
+      'value': 0},
+      // like two
+      {'object_id': template.object2,
+      'timestamp': currentDate(0),
+      'value': 1}]
+  },
+  'likeAndUnlikeOne' : {
+    'api_host': template.api_host,
+    'event_id': template.event_id,
+    'token': template.token,
+    'version': template.version,
+    'schedule_list': [
+      // like one
+      {'object_id': template.object1,
+      'timestamp': currentDate(-0.1),
+      'value': 0},
+      // and unlike it after
+      {'object_id': template.object1,
+      'timestamp': currentDate(0),
+      'value': 1}]
+  }
+}
+
+// var event = eventTemplate
+var event = events['firstSync']
 
 // Call the Lambda function
 likes.handler(event, context);
